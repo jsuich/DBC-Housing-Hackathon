60ba34e47d582adaf3b4864f1d1479a37dc47d9b
Rick Thomas <rickthomas1980@gmail.com>
2013-09-01 14:19:40 -0500

    added migration for housing file

--- db/migrate/20130901140119_create_houses.rb
+++ db/migrate/20130901140119_create_houses.rb
@@ -1,4 +1,15 @@
 class CreateHouses < ActiveRecord::Migration
   def change
+    create_table :houses do |t|
+      t.string :address
+      t.string :neighborhood
+      t.integer :number_of_rooms
+      t.integer :price
+      t.string :phone
+      t.string :email
+      t.text :description
+      t.integer :minimum_weeks
+      t.integer :maximum_weeks
+    end
   end
 end
